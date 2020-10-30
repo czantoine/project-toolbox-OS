303d60c0a3b4e78bf59c5438dbe002a63883171d
czantoine <ac11f@free.fr>
2020-10-30 13:59:04 +0100

    add work

---
+++ add_work.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -e "\n Add work file in course :"
+echo -e "\n What is semester you want go ?"
+read go_s
+echo -e "\n What is UE you want go ?"
+read go_ue
+echo -e "\n What is MO you want go ?"
+read go_mo
+echo -e "\n What is course you want go ?"
+read go_c
+echo -e "\n What is name of this new work?"
+read name_file
+
+touch $go_s/$go_ue/$go_mo/$go_c/$name_file
+
+touch $go_s/$go_ue/$go_mo/$go_c/progression.info
+
+echo "$name_file : 0%" >> $go_s/$go_ue/$go_mo/$go_c/progression.info
+
