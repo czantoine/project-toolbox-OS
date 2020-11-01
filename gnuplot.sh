f661609ebecfd195aa00cb6b0683334a1d262cf3
czantoine <ac11f@free.fr>
2020-11-01 15:29:37 +0100

    workfine

--- gnuplot.sh
+++ gnuplot.sh
@@ -16,10 +16,14 @@ read go_c
 
 if [[ $fonc = 1 ]]
 then
-	test=$($go_s/$go_ue/$go_mo/$go_c/progression.info)
-	gnuplot -persist -e 'set terminal canva ; set grid ; set style data histograms ; set style fill solid 1.00 border -1 ; set title "Work progress" ; set ylabel "Pourcentage" ;set xtic out rotate by 45; set xtic font ",12" ;set yrange [0:100];set output "test.html"; plot "'${test}'" using 2:xtic(1) title "Avancement"'
-	xdg-open test.html
+	user=$USER
+	cd $go_s/$go_ue/$go_mo/$go_c/
+	cp progression.info /home/$user/
+	cd --
+	gnuplot -persist -e 'set terminal canva ; set grid ; set style data histograms ; set style fill solid 1.00 border -1 ; set title "Work progress" ; set ylabel "Pourcentage" ;set xtic out rotate by 45; set xtic font ",12" ;set yrange [0:100];set output "test.html"; plot "progression.info" using 2:xtic(1) title "Avancement"'
+	firefox test.html
 	rm test.html
+	rm progression.info
 
 elif [[ $fonc = 2 ]]
 then
