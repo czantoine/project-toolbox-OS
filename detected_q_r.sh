9846e63683b751a5a09c8e3ff657a2d206bd82a0
czantoine <ac11f@free.fr>
2020-11-02 11:59:45 +0100

    update

--- detected_q_r.sh
+++ detected_q_r.sh
@@ -4,8 +4,7 @@ all_name=$(grep 'student' /etc/group | sort)
 clear_group=$(echo $all_name | sed 's/.*://')
 
 list=$(echo $clear_group | tr "," "\n")
-
-#compter le nombre de la liste pour changer x
+cmpt_list=$(echo $list | wc -w)
 
 V=($list)
 
@@ -13,15 +12,38 @@ for i in "${!V[@]}"; do
     printf 'V[%s] = %s\n' "$i" "${V[i]}"
 done
 
-for ((i = 0 ; i < $x ; i++)); do
+for ((i = 0 ; i < $cmpt_list ; i++)); do
 
-	directory=$(sudo find /home/${V[i]} -name "progression.info")
+	directory=$(sudo find /home/${V[i]} -name "question_remark.txt")
 	uu=$(echo $directory | tr " " "\n")
+	cmpt_uu=$(echo $uu | wc -w)
 	D=($uu)
 
-	#compter le nombre de directory pour set i
+	for k in "${!D[@]}"; do
+    		printf 'D[%s] = %s\n' "$k" "${D[k]}"
+	done
 
-	for i in "${!D[@]}"; do
-    		printf 'D[%s] = %s\n' "$i" "${D[i]}"
+	cd
+	cd /home/${V[i]}/
+	number_line_file=$(wc -l < semester_sheet.txt)
+	for((l = 1 ; l <= number_line_file ; l++));do
+		ligne=$(sed -n "${l},1p" semester_sheet.txt)
+		name_teacher=$(echo $ligne | sed 's/.*;TEACH://; s/;EMAIL:.*//')
+		email=$(echo $ligne | sed 's/.*;EMAIL://; s/;TP:.*//')
+		T=($name_teacher)
+		for c in "${!T[@]}"; do
+    			printf 'T[%s] = %s\n' "$c" "${T[c]}"
+			diff --brief <(sudo sort ${D[c]}) <(sudo sort /home/${T[c]}/question_remark.txt) >/dev/null
+			comp_value=$?
+			if [ $comp_value -eq 1 ]
+			then
+    				echo "do something because they're different"
+				sudo cp ${D[c]} /home/${T[c]}/question_remark.txt
+				echo "copie"
+				mail -s "Update Question & Remark" $email <<< 'Hello ! New file have been detected check your repo'
+			else
+  				echo "do something because they're identical"
+			fi
+		done
 	done
 done
