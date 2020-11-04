d78f8f4f08088126f5d772c0e291d6bd9487831d
czantoine <ac11f@free.fr>
2020-11-04 21:28:36 +0100

    update

--- detected_grade.sh
+++ detected_grade.sh
@@ -2,9 +2,14 @@
 
 user=$USER
 
+echo -e "\nWhat is username of student?"
+read student
+
+cd /home/$student
+
 email_student_grade=$( cat .email )
 
-directory=$(find /home/$user -name "grade.info")
+directory=$(sudo find /home/$student -name "grade.info")
 uu=$(echo $directory | tr " " "\n")
 cmpt_uu=$(echo $uu | wc -w)
 D=($uu)
@@ -13,7 +18,7 @@ for k in "${!D[@]}"; do
 done
 
 cd
-cd /home/$user/
+cd /home/$student/
 for((l = 0 ; l < cmpt_uu ; l++));do
 	for c in "${!D[@]}"; do
                         printf 'D[%s] = %s\n' "$c" "${D[c]}" > /dev/null
@@ -21,13 +26,12 @@ for((l = 0 ; l < cmpt_uu ; l++));do
                         comp_value=$?
                         if [ $comp_value -eq 1 ]
                         then
-				cp /home/$user/grade.conf /home/$user/.grade_back.conf
+				sudo cp /home/$student/grade.conf /home/$student/.grade_back.conf
                                 mail -s "New grade" $email_student_grade <<< 'Hello ! New grade add in your course'
                         else
                                 echo ""
                         fi
                 done
-
 
 done
 
